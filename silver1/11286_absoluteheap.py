cacf647a285d9f55e6847d53bdfaa7f705d70b99
723poil <75459370+723poil@users.noreply.github.com>
2021-09-15 09:02:52 +0900

    11286_absoluteheap

--- silver1/11286_absoluteheap.py
+++ silver1/11286_absoluteheap.py
@@ -1,74 +1,20 @@
 import sys
-from collections import deque
+import heapq
 
-absq = deque([0])
+input = sys.stdin.readline
 
-class absh:
-    def __init__(self):
-        self.count = 0
-    
-    def insert(self, data):
-        if  self.count == 0:
-            absq.append(data)
-            self.count += 1
+N=int(input())
+
+ah = []
+
+for i in range(N):
+    x = int(input())
+
+    if x != 0:
+        heapq.heappush(ah, (abs(x), x, i))
+    else:
+        if ah:
+            print(heapq.heappop(ah)[1])
         else:
-            temp = data
-            absq.append(temp)
-            self.count += 1
-
-            child = self.count
-            parent = child // 2
-
-            while child != 1 :
-                if abs(temp) < abs(absq[parent]) or (abs(temp) == abs(absq[parent]) and temp < 0 and absq[parent] > 0):
-                    absq[child] = absq[parent]
-                    child = parent
-                    parent = parent // 2
-                else :
-                    break
-
-            absq[child] = temp
+            print(0)
     
-    def hpop(self):
-        if self.count == 0:
-            return 0
-        
-        data = absq[1]
-        temp = absq[self.count]
-        absq.pop()
-        self.count -= 1
-
-        if self.count == 0:
-            return data
-
-        parent = 1
-        child = 2
-
-        while child <= self.count and self.count > 1:
-            if child < self.count :
-                if abs(absq[child]) > abs(absq[child+1]) or (abs(absq[child] == abs(absq[child+1]) and absq[child+1] < 0 and absq[child] > 0)):
-                    child += 1
-            if abs(temp) < abs(absq[child]) or (abs(temp) == abs(absq[child]) and temp < 0): return data
-            
-            absq[parent] = absq[child]
-            parent = child
-            child *= 2
-        
-        absq[parent] = temp
-
-        return data
-
-            
-
-if __name__ == '__main__':
-    N = int(sys.stdin.readline())
-
-    heap = absh()
-
-    for i in range(N):
-        a = int(sys.stdin.readline())
-
-        if a == 0:
-            print(heap.hpop())
-        else:
-            heap.insert(a)
